a9eb0f41d1ea7ecd63b51032ffb2f3cac8fd5107
dulee <mydulee06@gmail.com>
2023-08-27 22:44:07 +0000

    Update

--- onrobot_rg_control/nodes/OnRobotRGTcpNode.py
+++ onrobot_rg_control/nodes/OnRobotRGTcpNode.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+import math
+
 import rospy
 import onrobot_rg_control.baseOnRobotRG
 import onrobot_rg_modbus_tcp.comModbusTcp
 from std_srvs.srv import Trigger, TriggerResponse
 from onrobot_rg_control.msg import OnRobotRGInput
 from onrobot_rg_control.msg import OnRobotRGOutput
+from sensor_msgs.msg import JointState
 
 
 class OnRobotRGTcp:
@@ -32,9 +35,22 @@ class OnRobotRGTcp:
         # Connecting to the ip address received as an argument
         self.gripper.client.connectToDevice(ip, port, changer_addr)
 
+        self.joint_names = ["finger_joint", "left_inner_knuckle_joint", "left_inner_finger_joint", 
+                            "right_outer_knuckle_joint", "right_inner_knuckle_joint", "right_inner_finger_joint"]
+        self.mimic_ratios = [1, -1, 1, -1, -1, 1]
+
+        self.L1 = 0.1389215
+        self.theta1 = 1.3963
+        self.L3 = 0.08
+        self.theta3 = 0.93766
+        self.dy = -0.0245
+        self.dz = 0.055
+
         # The Gripper status is published on the topic 'OnRobotRGInput'
         self.pub = rospy.Publisher(
             'OnRobotRGInput', OnRobotRGInput, queue_size=1)
+        self.joint_states_pub = rospy.Publisher(
+            'joint_states', JointState, queue_size=5)
 
         # The Gripper command is received from the topic 'OnRobotRGOutput'
         rospy.Subscriber('OnRobotRGOutput',
@@ -48,6 +64,10 @@ class OnRobotRGTcp:
             self.restartPowerCycle)
 
         self.mainLoop()
+        
+    def widthToJointValue(self, width):
+        j_value = math.acos(((width/2) - self.dy - self.L1 * math.cos(self.theta1)) / self.L3) - self.theta3
+        return j_value
 
     def restartPowerCycle(self, request):
         """ Restarts the power cycle of the gripper. """
@@ -66,7 +86,16 @@ class OnRobotRGTcp:
         while not rospy.is_shutdown():
             # Getting and publish the Gripper status
             status = self.gripper.getStatus()
+
             self.pub.publish(status)
+
+            msg = JointState()
+            msg.header.stamp = rospy.Time.now()
+            msg.name = self.joint_names
+            if status.gWDF < 1600:
+                j_value = self.widthToJointValue(status.gWDF / 10000)
+                msg.position = [j_value * ratio for ratio in self.mimic_ratios]
+                self.joint_states_pub.publish(msg)
 
             rospy.sleep(0.05)
             # Sending the most recent command
